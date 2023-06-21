f7dbe76a1b5aaeb8f2003fc222bcb4c90a6e3997
Мялкин Алексей Николаевич <a.myalkin@tinkoff.ru>
2023-06-21 17:11:56 +0300

    Add AutoData test

--- tests/Tests.AutoFixture.Xunit2/SimpleTests.cs
+++ tests/Tests.AutoFixture.Xunit2/SimpleTests.cs
@@ -1,11 +1,21 @@
+using AutoFixture.Xunit2;
+using FluentAssertions;
+
 namespace Tests.AutoFixture.Xunit2;
 
 public class SimpleTests
 {
     [Theory]
     [InlineData("one")]
-    public void Test1(string value)
+    public void InlineData(string value)
     {
-        Assert.Equal("one", value);
+        value.Should().Be("one");
+    }
+
+    [Theory]
+    [AutoData]
+    public void AutoData(string value)
+    {
+        value.Should().NotBeNullOrEmpty(value);
     }
 }
